7e30ae9351eeb935483b15db3866f4b3c9db4abd
léo fasano <leo.fasano@hortis.ch>
2017-03-27 16:55:39 +0200

    Rejected commit: Default

--- app/src/main/java/com/example/fasanol/outerspacemanager/GalaxyActivity.java
+++ app/src/main/java/com/example/fasanol/outerspacemanager/GalaxyActivity.java
@@ -3,9 +3,12 @@ package com.example.fasanol.outerspacemanager;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -25,29 +28,47 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.example.fasanol.outerspacemanager.R.id.recyclerView;
 
-public class GalaxyActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class GalaxyActivity extends Fragment implements AdapterView.OnItemClickListener {
     private Retrofit ret = new Retrofit.Builder()
             .baseUrl("https://outer-space-manager.herokuapp.com")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
     private ProgressDialog mProgressDialog;
-    private ListView listUser;
     private ArrayList<User> users;
     private SharedPreferences settings;
     private String token;
-    private ArrayList<String> listUserName;
+    public ArrayList<String> listUserName;
+    public ListView lvFragment;
+
+    /*protected void onCreate(Bundle savedInstanceState) {
+
+    }*/
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("selectedUserToViewReports", listUserName.get(position));
+        editor.commit();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_galaxy,container);
+        lvFragment = (ListView)v.findViewById(R.id.listUser);
+        lvFragment.setOnItemClickListener((GalaxyFragment)getActivity());
+
+        return v;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_galaxy);
-        mProgressDialog = ProgressDialog.show(this, "",
+        mProgressDialog = ProgressDialog.show(getActivity(), "",
                 "Loading users", true);
 
-        this.settings = getSharedPreferences("apiSettings", 0);
+        this.settings = getActivity().getSharedPreferences("apiSettings", 0);
         this.token = settings.getString("token", "");
-        this.listUser = (ListView) findViewById(R.id.listUser);
-        this.listUser.setOnItemClickListener(this);
 
         UserInterface service = ret.create(UserInterface.class);
         Call<UserResponse> request = service.getAllUsers(token);
@@ -55,11 +76,11 @@ public class GalaxyActivity extends AppCompatActivity implements AdapterView.OnI
             @Override
             public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
                 mProgressDialog.dismiss();
-                if(response.isSuccessful())
+                if(response.isSuccessful() && getActivity() != null)
                 {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(GalaxyActivity.this,
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                             android.R.layout.simple_list_item_1, response.body().getUsersInfos());
-                    listUser.setAdapter(adapter);
+                    lvFragment.setAdapter(adapter);
 
                     listUserName = response.body().getUserNames();
 
@@ -77,12 +98,8 @@ public class GalaxyActivity extends AppCompatActivity implements AdapterView.OnI
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("selectedUserToViewReports", listUserName.get(position));
-        editor.commit();
-
-        Intent myIntent = new Intent(getApplicationContext(), userDetailsActivity.class);
-        startActivity(myIntent);
+    public void onDestroy() {
+        super.onDestroy();
+        this.mProgressDialog.dismiss();
     }
 }
